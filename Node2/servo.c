1a5bfd357ffbdeecabe2ccd0da86d6dddab3b270
unknown <geirhei@itk-d663.win.ntnu.no>
2015-11-12 21:02:48 +0100

    Servo changed to register 3

--- Node2/servo.c
+++ Node2/servo.c
@@ -12,25 +12,25 @@
 void timer1_init(void)
 {
 	// Set up timer in Fast PWM mode 14
-	TCCR1A |= (1 << COM1A1)|(1 << WGM11);
+	TCCR3A |= (1 << COM1A1)|(1 << WGM11);
 	
 	// Set up timer with prescaler = 64 and PWM fast mode 14
-	TCCR1B |= (1 << WGM13)|(1 << WGM12)|(1 << CS11)|(1 << CS10);
+	TCCR3B |= (1 << WGM13)|(1 << WGM12)|(1 << CS11)|(1 << CS10);
 	
 	// Set up timer with TOP value = 5000 (0x1388)
-	ICR1 = 0x1388;
+	ICR3 = 0x1388;
 	
 	// initialize counter
-	TCNT1 = 0;
+	TCNT3 = 0;
 	
 	// initialize compare value (servo to middle, 0x0177)
-	OCR1A = 0x0177; //middle
+	OCR3A = 0x0177; //middle
 	
 	// Enable compare interrupt
-	TIMSK1 |= (1 << OCIE1A);
+	TIMSK3 |= (1 << OCIE1A);
 	
 	// Set PWM port as output
-	DDRB = (1 << PB5);
+	DDRE = (1 << PE3);
 }
 
 void SERVO_write(position_t pos)
@@ -43,12 +43,12 @@ void SERVO_write(position_t pos)
 	
 	// Update compare register
 	//printf("new_pos: %d\n", new_pos);
-	OCR1A = new_pos;
+	OCR3A = new_pos;
 }
 
 /* Interrupt handler for TIMER1 compare */
-ISR(TIMER1_COMPA_vect) {
+ISR(TIMER3_COMPA_vect) {
 
 	// pin toggle
-	PORTB ^= (1 << PB5);
+	PORTE ^= (1 << PE3);
 }
